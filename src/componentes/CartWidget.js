a4932067538ce1e602da082f5dfb26d9a0fd0ebe
Kabel <45271283+Kabel98@users.noreply.github.com>
2022-08-24 12:36:33 -0300

    mejora num

--- src/componentes/CartWidget.js
+++ src/componentes/CartWidget.js
@@ -1,14 +1,17 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaShoppingCart } from "react-icons/fa";
-import { faFont } from "@fortawesome/free-solid-svg-icons";
-import { faFontAwesome } from "@fortawesome/free-solid-svg-icons";
+import './CartWidget.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 function CartWidget(){
 return(
-   <span className= "fa-layers fa-fw fa-layers-top-left">
-        <FaShoppingCart size={35} color="rgb(242, 228, 199)"/>
-        <span class="fa-layers-counter" style={{color:"white", fontWeight:900}}>5</span>
+    
+    <div className="fa-2x">
+        <span className= "fa-layers fa-fw" >
+            <link rel="stylesheet" href="CartWidget.css"></link>
+            <i class="fas fa-shopping-cart" style={{color:"#83B799"}}></i>
+            <span className="fa-layers-counter" style={{background:"tomato", color:"white", fontSize:45}}>5</span>
     </span>
+    </div>
    
 )
 }
